4d2d0864e1813828c074e0dfb0f1b07fbea2ce37
Amila Welihinda <amilajack@gmail.com>
2016-08-20 20:05:43 -0700

    Added optimization messages

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const execSync = require('child_process').execSync;
 
+// @TODO: Currently, flow runs all its checks for each file. That means that
+//        O(n) = n^2, where a is the number of files flow has to check. A
+//        possible optimization is watching the source files to check if
+//        any file has changed. If a file did change, run flow and cache
+
 
 module.exports = {
   rules: {
@@ -11,23 +16,34 @@ module.exports = {
       const loo = collected.toString();
       const lee = JSON.parse(collected);
 
+
       function collectFlowErrors(node) {
 
-        try {
-          const found = lee.find(each => {
-            return (
-              each.start === node.loc.start.line &&
-              each.path === context.getFilename()
-            )
-          });
+        if (Array.isArray(lee)) {
+          try {
+            const found = lee.find(each => {
 
-          if (found) {
-            return context.report(node, found.message);
+              if (each.path === context.getFilename()) {
+                console.log(each.path === context.getFilename());
+                console.log(each.start, node.loc.start.line);
+                console.log(context.getFilename());
+              }
+              return (
+                each.start === node.loc.start.line &&
+                each.path === context.getFilename()
+              )
+            });
+
+            if (found) {
+              return context.report(node, found.message);
+            }
+          } catch (err) {
+            console.log(err);
           }
-        } catch (err) {
-          console.log(err);
         }
       }
+
+      // @TODO: Get all the AST node types of the flow errors and only check those
 
       return {
         Program: collectFlowErrors,
@@ -35,10 +51,14 @@ module.exports = {
         BlockStatement: collectFlowErrors,
         WhileStatement: collectFlowErrors,
         ForStatement: collectFlowErrors,
+        BlockStatement: collectFlowErrors,
+        ExpressionStatement: collectFlowErrors,
         ForInStatement: collectFlowErrors,
         ForOfStatement: collectFlowErrors,
         DoWhileStatement: collectFlowErrors,
         IfStatement: collectFlowErrors,
+        Identifier: collectFlowErrors,
+        FunctionDeclaration: collectFlowErrors,
         VariableDeclaration: collectFlowErrors,
         ObjectExpression: collectFlowErrors,
         ArrayExpression: collectFlowErrors,
