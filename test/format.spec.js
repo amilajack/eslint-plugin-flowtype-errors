58afa3ab69c5b2af2e5f6f87f46b7d045a733649
Amila Welihinda <amilajack@gmail.com>
2016-09-24 17:47:33 -0700

    Removed hardcoded 'path' property from tests

--- test/format.spec.js
+++ test/format.spec.js
@@ -4,44 +4,53 @@ import { expect } from 'chai';
 
 describe('Format', () => {
   const collected = execSync('node ./dist/collect.js');
-  const parsedJSON = JSON.parse(collected);
+  const parsedJSONArray = JSON.parse(collected);
 
-  it('should return exact formatting errors', done => {
-    expect(parsedJSON).to.eql([
+  it('should have expected properties', done => {
+    expect(parsedJSONArray.map(e => (e === false
+        ? false
+        : {
+          end: e.end,
+          message: e.message,
+          start: e.start
+        }
+    ))).to.eql([
       {
         message: 'This type is incompatible with the expected return type of string',
-        path: '/Users/amila/Documents/Projects/eslint-plugin-flowtype-errors/test/code.example.js',
         start: 6,
         end: 6
       },
       false,
       {
         message: 'This type is incompatible with the expected return type of boolean',
-        path: '/Users/amila/Documents/Projects/eslint-plugin-flowtype-errors/test/code.example.js',
         start: 5,
         end: 5
       },
       {
         message: 'Could not resolve name identifier `x`',
-        path: '/Users/amila/Documents/Projects/eslint-plugin-flowtype-errors/test/code.example.js',
         start: 9,
         end: 9
       },
       false,
       {
         message: 'This type is incompatible with string',
-        path: '/Users/amila/Documents/Projects/eslint-plugin-flowtype-errors/test/code.example.js',
         start: 16,
         end: 16
       },
       false,
       {
         message: 'This type is incompatible with number',
-        path: '/Users/amila/Documents/Projects/eslint-plugin-flowtype-errors/test/code.example.js',
         start: 16,
         end: 16
       }
     ]);
+
+    for (const e of parsedJSONArray) {
+      if (e !== false) {
+        expect(e.path).to.be.a('string').that.includes('code.example.js');
+      }
+    }
+
     done();
   });
 });
