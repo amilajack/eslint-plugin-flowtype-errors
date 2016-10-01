11bf2cb0b257dd951d2fbd68178d3b4de7ef9734
Amila Welihinda <amilajack@gmail.com>
2016-09-30 22:05:47 -0700

    Added initial react component prop test

--- test/exactFormat.js
+++ test/exactFormat.js
@@ -187,4 +187,22 @@ export default [{
   "message": "function: This type cannot be added to 'string' ",
   "start": 21,
   "type": "default"
+},
+{
+  "end": 12,
+  "loc": {
+    "end": {
+      "column": 3,
+      "line": 12,
+      "offset": 170
+    },
+    "start": {
+      "column": 10,
+      "line": 9,
+      "offset": 120
+    }
+  },
+  "message": "property `lastName`: Property not found in 'props of React element `Foo`' ",
+  "start": 9,
+  "type": "default"
 }]
