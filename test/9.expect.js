e6823cebdcdce59e858bea1c502cc28fd1a4a1f2
Simon Lydell <simon.lydell@gmail.com>
2017-03-24 08:18:05 +0100

    Add failing test case for #68

---
+++ test/9.expect.js
@@ -0,0 +1,21 @@
+// TODO: Not sure if this actually is the wanted output.
+export default [
+  {
+    end: 7,
+    loc: {
+      end: {
+        column: 62,
+        line: 7,
+        offset: 128
+      },
+      start: {
+        column: 49,
+        line: 7,
+        offset: 114
+      }
+    },
+    message: "property `name`:  Property not found in 'props of React element `Hello`'. See ./test/9.example.js:9",
+    start: 7,
+    type: 'default'
+  }
+];
