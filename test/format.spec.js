c39b7ccf909b6dbb7ba47864966dd9f09698287a
Amila Welihinda <amilajack@users.noreply.github.com>
2017-04-24 05:37:36 -0700

    Fixed indentation of format.spec.js

--- test/format.spec.js
+++ test/format.spec.js
@@ -73,35 +73,35 @@ const codebases = [
 ];
 
 const eslintConfig = `
-var Module = require('module');
-var path = require('path');
-var original = Module._resolveFilename;
+  var Module = require('module');
+  var path = require('path');
+  var original = Module._resolveFilename;
 
-// Hack to allow eslint to find the plugin
-Module._resolveFilename = function(request, parent, isMain) {
-  if (request === 'eslint-plugin-flowtype-errors') {
-    return path.resolve('../../../dist/index.js');
-  }
-  return original.call(this, request, parent, isMain);
-};
-
-module.exports = {
-  parser: 'babel-eslint',
-  root: true, // Make ESLint ignore configuration files in parent folders
-  env: {
-    node: true,
-    es6: true
-  },
-  plugins: ['flowtype-errors'],
-  settings: {
-    'flowtype-errors': {
-      flowDir: './subdir'
+  // Hack to allow eslint to find the plugin
+  Module._resolveFilename = function(request, parent, isMain) {
+    if (request === 'eslint-plugin-flowtype-errors') {
+      return path.resolve('../../../dist/index.js');
     }
-  },
-  rules: {
-    'flowtype-errors/show-errors': 2
-  }
-};
+    return original.call(this, request, parent, isMain);
+  };
+
+  module.exports = {
+    parser: 'babel-eslint',
+    root: true, // Make ESLint ignore configuration files in parent folders
+    env: {
+      node: true,
+      es6: true
+    },
+    plugins: ['flowtype-errors'],
+    settings: {
+      'flowtype-errors': {
+        flowDir: './subdir'
+      }
+    },
+    rules: {
+      'flowtype-errors/show-errors': 2
+    }
+  };
 `;
 
 describe('Check codebases', () => {
