7015fc7dacaa55eec29c62cd1e679ad0b07900be
João Mota <jdmota@outlook.pt>
2018-08-28 22:09:01 -0700

    New config lookup (#125)
    * Breaking change: New config lookup
    
    * Fix tests
    
    * Increase test timeout
    
    * Don't strip eof (for now)

--- src/get-program.js
+++ src/get-program.js
@@ -5,17 +5,16 @@ type Pos = {
   column: number
 };
 
-type Loc = {
+export type Loc = {
   start: Pos,
   end: Pos
 };
 
-type Program = { text: string, loc: Loc, offset: Pos };
+export type Program = { text: string, loc: Loc, offset: Pos };
 
-export default function( source: Object, node: Object ): ?Program {
-
+export default function(source: Object, node: Object): ?Program {
   // Ignore if body is empty.
-  if ( node.body.length === 0 ) {
+  if (node.body.length === 0) {
     return;
   }
 
@@ -27,17 +26,20 @@ export default function( source: Object, node: Object ): ?Program {
   // With babel-eslint, program.loc.start.column is always 0,
   // workaround it by using the first node of the body to get the offset.
 
-  if ( comments0 ) {
-    start = node.range[0] < comments0.range[0] ? {
-      line: body0.loc.start.line,
-      column: body0.loc.start.column
-    } : {
-      line: comments0.loc.start.line,
-      column: comments0.loc.start.column
-    };
+  if (comments0) {
+    start =
+      node.range[0] < comments0.range[0]
+        ? {
+            line: body0.loc.start.line,
+            column: body0.loc.start.column
+          }
+        : {
+            line: comments0.loc.start.line,
+            column: comments0.loc.start.column
+          };
     range = [
-      Math.min( node.range[0], comments0.range[0] ),
-      Math.max( node.range[1], node.comments[node.comments.length - 1].range[1] )
+      Math.min(node.range[0], comments0.range[0]),
+      Math.max(node.range[1], node.comments[node.comments.length - 1].range[1])
     ];
   } else {
     start = {
@@ -48,7 +50,7 @@ export default function( source: Object, node: Object ): ?Program {
   }
 
   return {
-    text: source.text.slice( range[0], range[1] ),
+    text: source.text.slice(range[0], range[1]),
     loc: {
       start,
       end: start
